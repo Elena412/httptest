4e5ee81d15fc0210ede9f20022c0008be08e5e1e
sedmess <n1ks@inbox.ru>
2014-11-07 22:49:27 +0300

    универсализация XMessage для xml и json

--- src/ru/n1ks/XMessage.java
+++ src/ru/n1ks/XMessage.java
@@ -1,6 +1,10 @@
 package ru.n1ks;
 
 import jdk.nashorn.internal.runtime.ParserException;
+import org.json.simple.*;
+
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -19,37 +23,33 @@ import java.io.*;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 
+final class XMap {
+
+}
+
 @SuppressWarnings("FieldCanBeLocal")
 final class XMessage {
     private DocumentBuilderFactory dbf;
     private DocumentBuilder db;
 
+    private boolean parsed = false;
+
+    /* xml in */
     private String type = null;
     private String data = null;
 
-    public String getData() {
-        return data;
-    }
+    /* json in */
+    private String status = null;
+    private String position = null;
+    private String sizeX = null;
+    private String sizeY = null;
+    private String squareSize = null;
+    private XMap map = null;
 
-    public String getType() {
-        return type;
-    }
-
-    public XMessage(InputStream is) throws ParserConfigurationException, IOException, SAXException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
-        String line;
-        StringBuilder sb = new StringBuilder();
-        while ((line = br.readLine()) != null){
-            sb.append(line);
-        }
-        String xml = URLDecoder.decode(sb.toString(), "UTF-8");
-        Main.showMessage(">----------------xml data begins--------------------");
-        Main.showMessage(xml);
-        Main.showMessage(">----------------xml data ends----------------------");
-        is = new ByteArrayInputStream(xml.getBytes());
+    private void XMLParse(InputStream is) throws ParserConfigurationException, IOException, SAXException {
         dbf = DocumentBuilderFactory.newInstance();
         db = dbf.newDocumentBuilder();
-        Main.showMessage(": try to parse...");
+        Main.showMessage(": try to parse xml...");
         Document xmlDoc = db.parse(is);
         Main.showMessage(": ...done.");
         Node node = xmlDoc.getDocumentElement();
@@ -65,10 +65,46 @@ final class XMessage {
         }
         if (type == null || data == null)
             throw new ParserException("Invalid xml.");
+
+    }
+
+    public void JSONParse(InputStream is) throws IOException, ParseException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+        String line;
+        StringBuilder sb = new StringBuilder();
+        while ((line = br.readLine()) != null){
+            sb.append(line);
+        }
+        JSONParser parser = new JSONParser();
+        JSONObject doc = (JSONObject) parser.parse(sb.toString());
+
+        /*TODO try to parse STATUS ret*/
+
+    }
+
+    public XMessage(InputStream is) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+        String line;
+        StringBuilder sb = new StringBuilder();
+        while ((line = br.readLine()) != null){
+            sb.append(line);
+        }
+        String xml = URLDecoder.decode(sb.toString(), "UTF-8");
+        Main.showMessage(">--------------------data begins--------------------");
+        Main.showMessage(xml);
+        Main.showMessage(">--------------------data ends----------------------");
+        is = new ByteArrayInputStream(xml.getBytes());
+        try {
+            XMLParse(is);
+        } catch (ParserConfigurationException | SAXException e) {
+            Main.showMessage(e.getMessage());
+            return;
+        }
+        parsed = true;
     }
 
     public String getReplyEncoded() throws TransformerException, UnsupportedEncodingException {
-        if (type == null || data == null)
+        if (!parsed)
             return null;
         Document resDoc = db.newDocument();
         Element root = resDoc.createElement("Response");
@@ -82,9 +118,9 @@ final class XMessage {
         TransformerFactory tf = TransformerFactory.newInstance();
         Transformer transformer = tf.newTransformer();
         transformer.transform(domSource, result);
-        Main.showMessage("<----------------xml data begins--------------------");
+        Main.showMessage("<--------------------data begins--------------------");
         Main.showMessage(writer.toString());
-        Main.showMessage("<----------------xml data ends----------------------");
+        Main.showMessage("<--------------------data ends----------------------");
         return URLEncoder.encode(writer.toString(), "UTF-8");
     }
 }
